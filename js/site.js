25256ecde29a02226d7f7ed0c8f0ce04b2f18741
Corbin Meier <corbinivon@gmail.com>
2023-04-10 00:28:20 +0000

    Fixed error. Prod. does not load editables.

--- js/site.js
+++ js/site.js
@@ -1,40 +1,40 @@
-// New Code to fix #1
 const edit_Button = document.getElementById("edit-button");
 const preview_Button = document.getElementById("preview-button");
 const textarea_PostArea = document.getElementById("textarea-post-area");
 const preview_PostArea = document.getElementById("preview-post-area");
 const submit_togglable = document.getElementById("login-togglable");
 
-edit_Button.addEventListener("click", () => {
-  if (submit_togglable != null)
-    submit_togglable.style.display = "block";
-  textarea_PostArea.value = preview_PostArea.innerHTML;
+if (edit_Button != null){
+  edit_Button.addEventListener("click", () => {
+    if (submit_togglable != null)
+      submit_togglable.style.display = "block";
+    textarea_PostArea.value = preview_PostArea.innerHTML;
 
-  preview_PostArea.style.display = "none";
-  textarea_PostArea.style.display = "block";
+    preview_PostArea.style.display = "none";
+    textarea_PostArea.style.display = "block";
 
-  preview_Button.style.display = "block";
-  edit_Button.style.display = "none";
+    preview_Button.style.display = "block";
+    edit_Button.style.display = "none";
 
-  textarea_PostArea.style.height = "auto";
-  textarea_PostArea.style.height = `${textarea_PostArea.scrollHeight}px`;
-});
-preview_Button.addEventListener("click", () => {
-  if (submit_togglable != null)
-    submit_togglable.style.display = "none";
-  preview_PostArea.innerHTML = textarea_PostArea.value;
+    textarea_PostArea.style.height = "auto";
+    textarea_PostArea.style.height = `${textarea_PostArea.scrollHeight}px`;
+  });
+  preview_Button.addEventListener("click", () => {
+    if (submit_togglable != null)
+      submit_togglable.style.display = "none";
+    preview_PostArea.innerHTML = textarea_PostArea.value;
 
-  preview_PostArea.style.display = "block";
-  textarea_PostArea.style.display = "none";
+    preview_PostArea.style.display = "block";
+    textarea_PostArea.style.display = "none";
 
-  preview_Button.style.display = "none";
-  edit_Button.style.display = "block";
-});
-textarea_PostArea.addEventListener("input", () => {
-  textarea_PostArea.style.height = "auto";
-  textarea_PostArea.style.height = `${textarea_PostArea.scrollHeight}px`;
-});
-
+    preview_Button.style.display = "none";
+    edit_Button.style.display = "block";
+  });
+  textarea_PostArea.addEventListener("input", () => {
+    textarea_PostArea.style.height = "auto";
+    textarea_PostArea.style.height = `${textarea_PostArea.scrollHeight}px`;
+  });
+}
 
 const carousel_images = document.querySelectorAll('.carousel-image');
 let currentIndex = 0;
